708d21f9ffd9aa89a205140b2e8591aefcc37d4e
K900 <me@0upti.me>
2024-08-29 17:40:00 +0300

    kdePackages.kirigami: propagate kitemmodels

--- pkgs/kde/frameworks/kirigami/default.nix
+++ pkgs/kde/frameworks/kirigami/default.nix
@@ -5,6 +5,7 @@
   qttools,
   qtdeclarative,
   qt5compat,
+  kitemmodels,
   qqc2-desktop-style,
 }:
 # Kirigami has a runtime dependency on qqc2-desktop-style,
@@ -19,7 +20,7 @@ let
 
     extraNativeBuildInputs = [qtsvg qttools];
     extraBuildInputs = [qtdeclarative];
-    extraPropagatedBuildInputs = [qt5compat];
+    extraPropagatedBuildInputs = [qt5compat kitemmodels];
   };
 in stdenv.mkDerivation {
   pname = "kirigami-wrapped";
